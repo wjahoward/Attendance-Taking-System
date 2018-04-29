d6159e98368fb71651b169e79a813b6f888a5eb6
yungjas <jasmine-lim88@hotmail.com>
2018-04-29 23:24:46 +0800

    ibeacon android transmitter

---
+++ IBeaconAT/IBeaconAT/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using AltBeaconOrg.BoundBeacon;
+using Android.Bluetooth.LE;
+
+namespace IBeaconAT
+{
+    [Activity(Label = "IBeaconAT", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private BeaconManager beaconManager;
+        public AdvertiseCallback advertiseCallback; 
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //var beacon1 = new AltBeacon.Builder().SetId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
+            //    .SetId2("1").SetId3("1").SetRssi(-55).SetTxPower(-55).Build();
+
+            beaconManager = BeaconManager.GetInstanceForApplication(this);
+
+            BeaconTransmitter bTransmitter = new BeaconTransmitter();
+            bTransmitter.Transmit();
+        }
+    }
+}
+
