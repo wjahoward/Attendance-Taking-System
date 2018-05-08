121885cdcce2a64dbbf2f0e2b25c2b04f3dbe466
Phobus123 <sngyiekit@yahoo.com.sg>
2018-05-09 03:06:21 +0800

    commit

--- BeaconTest/DataAccess.cs
+++ BeaconTest/DataAccess.cs
@@ -6,15 +6,9 @@ namespace BeaconTest
         public DataAccess()
         {
         }
-<<<<<<< .merge_file_0TNF0f
 
         public static string AuthenticationUrl = "testfyp.database.windows.net/api/Authentication?username=&password=";
         public static string LecturerPostUrl = "testfyp.database.windows.net/api/Lecturer";
         public static string StudentPostUrl = "testfyp.database.windows.net/api/Student";
     }
 }
-
-=======
-    }
-}
->>>>>>> .merge_file_fjguC0
