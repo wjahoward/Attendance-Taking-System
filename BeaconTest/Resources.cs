32f43dfd605dde9b228d580c6c77ef6e4254aa4e
Phobus123 <sngyiekit@yahoo.com.sg>
2018-05-08 22:21:52 +0800

    Added Resources.cs

---
+++ BeaconTest/Resources.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BeaconTest
+{
+    public static class Resources
+    {
+        public static string primaryColourHex = "#2BDDC3";
+        public static string primaryColourRGB = "(43, 221, 195)";
+    }
+}
